ba8fd3ccb985d5845801980175097e66215657b2
Andrew Paterson <andrew.paterson9@gmail.com>
2024-11-15 11:31:28 +0200

    Proper tests

--- test.js
+++ test.js
@@ -1,35 +1,192 @@
 const blockCombinator = require('./index');
-
-// const data = [
-//   {
-//     block: 'colours',
-//     items: ['red', { value: 'orange' }, 'blue'],
-//     required: true,
-//   },
-//   {
-//     block: 'numbers',
-//     items: ['1', '2'],
-//   },
-//   {
-//     block: 'shapes',
-//     items: ['circle', 'square'],
-//   },
-// ];
-const data = [
+const chalk = require('chalk');
+const testSchemas = [
   {
-    block: 'colours',
-    // include: ['1', 'circle'],
-    // items: ['red', { value: 'orange', ignozre: ['shapes', '1'] }],
-    items: ['red', { value: 'orange', include: ['1', 'circle'] }],
+    title: 'Basic usage',
+    testFunction() {
+      return blockCombinator([
+        {
+          block: 'colours',
+          items: ['red', 'orange'],
+        },
+        {
+          block: 'numbers',
+          items: ['1', '2'],
+        },
+        {
+          block: 'shapes',
+          items: ['circle', 'square'],
+        },
+      ]);
+    },
+    expectation: [
+      ['red', '1', 'circle'],
+      ['red', '1', 'square'],
+      ['red', '1'],
+      ['red', '2', 'circle'],
+      ['red', '2', 'square'],
+      ['red', '2'],
+      ['red', 'circle'],
+      ['red', 'square'],
+      ['orange', '1', 'circle'],
+      ['orange', '1', 'square'],
+      ['orange', '1'],
+      ['orange', '2', 'circle'],
+      ['orange', '2', 'square'],
+      ['orange', '2'],
+      ['orange', 'circle'],
+      ['orange', 'square'],
+      ['1', 'circle'],
+      ['1', 'square'],
+      ['2', 'circle'],
+      ['2', 'square'],
+    ],
   },
   {
-    block: 'numbers',
-    items: ['1', '2'],
+    title: 'Required blocks',
+    testFunction() {
+      return blockCombinator([
+        {
+          block: 'colours',
+          items: ['red', 'orange'],
+          required: true,
+        },
+        {
+          block: 'numbers',
+          items: ['1', '2'],
+        },
+        {
+          block: 'shapes',
+          items: ['circle', 'square'],
+        },
+      ]);
+    },
+    expectation: [
+      ['red', '1', 'circle'],
+      ['red', '1', 'square'],
+      ['red', '1'],
+      ['red', '2', 'circle'],
+      ['red', '2', 'square'],
+      ['red', '2'],
+      ['red', 'circle'],
+      ['red', 'square'],
+      ['orange', '1', 'circle'],
+      ['orange', '1', 'square'],
+      ['orange', '1'],
+      ['orange', '2', 'circle'],
+      ['orange', '2', 'square'],
+      ['orange', '2'],
+      ['orange', 'circle'],
+      ['orange', 'square'],
+    ],
   },
   {
-    block: 'shapes',
-    items: ['circle', 'square'],
+    title: 'Ignoring at block level',
+    testFunction() {
+      return blockCombinator([
+        {
+          block: 'colours',
+          items: ['red', 'orange'],
+          ignore: ['numbers', 'square'],
+        },
+        {
+          block: 'numbers',
+          items: ['1', '2'],
+        },
+        {
+          block: 'shapes',
+          items: ['circle', 'square'],
+        },
+      ]);
+    },
+    expectation: [
+      ['red', 'circle'],
+      ['orange', 'circle'],
+      ['1', 'circle'],
+      ['1', 'square'],
+      ['2', 'circle'],
+      ['2', 'square'],
+    ],
+  },
+  {
+    title: 'Ignoring at the item level',
+    testFunction() {
+      return blockCombinator([
+        {
+          block: 'colours',
+          items: ['red', { value: 'orange', ignore: ['shapes', '1'] }],
+        },
+        {
+          block: 'numbers',
+          items: ['1', '2'],
+        },
+        {
+          block: 'shapes',
+          items: ['circle', 'square'],
+        },
+      ]);
+    },
+    expectation: [
+      ['red', '1', 'circle'],
+      ['red', '1', 'square'],
+      ['red', '1'],
+      ['red', '2', 'circle'],
+      ['red', '2', 'square'],
+      ['red', '2'],
+      ['red', 'circle'],
+      ['red', 'square'],
+      ['orange', '2'],
+      ['1', 'circle'],
+      ['1', 'square'],
+      ['2', 'circle'],
+      ['2', 'square'],
+    ],
+  },
+  {
+    title: 'Including at the item level',
+    testFunction() {
+      return blockCombinator([
+        {
+          block: 'colours',
+          items: ['red', { value: 'orange', include: ['1', 'circle'] }],
+        },
+        {
+          block: 'numbers',
+          items: ['1', '2'],
+        },
+        {
+          block: 'shapes',
+          items: ['circle', 'square'],
+        },
+      ]);
+    },
+    expectation: [
+      ['red', '1', 'circle'],
+      ['red', '1', 'square'],
+      ['red', '1'],
+      ['red', '2', 'circle'],
+      ['red', '2', 'square'],
+      ['red', '2'],
+      ['red', 'circle'],
+      ['red', 'square'],
+      ['orange', '1', 'circle'],
+      ['orange', '1'],
+      ['orange', 'circle'],
+      ['1', 'circle'],
+      ['1', 'square'],
+      ['2', 'circle'],
+      ['2', 'square'],
+    ],
   },
 ];
 
-console.log(blockCombinator(data, { minLength: 2 }));
+testSchemas.forEach((testSchema) => {
+  const result = testSchema.testFunction();
+  if (JSON.stringify(result) !== JSON.stringify(testSchema.expectation)) {
+    console.log(chalk.red(`Test failed: ${testSchema.title}`));
+    console.log(`Expected: ${testSchema.expectation}`);
+    console.log(`Received: ${result}`);
+  } else {
+    console.log(chalk.green(`Test passed: ${testSchema.title}`));
+  }
+});
